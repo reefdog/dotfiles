84441d09c59cc5e15e8ea3f2743b032685ac9e96
Justin Reese <justin@justinreese.com>
2021-11-10 13:53:57 -0600

    Remove unnecessary comments

--- macos/install.sh
+++ macos/install.sh
@@ -1,8 +1,3 @@
-# The Brewfile handles Homebrew-based app and library installs, but there may
-# still be updates and installables in the Mac App Store. There's a nifty
-# command line interface to it that we can use to just install everything, so
-# yeah, let's do that.
-
 # Disabling this until I better understand all the options
 # echo "Setting up macOS defaults..."
 # sh -c "macos/set-defaults.sh"
